67e69cc1f2dca1b0f044998ae595add6d8dbc638
CHENGHSIEN Lo <aszx99370765@gmail.com>
2019-03-29 15:13:48 +0800

    add loop

--- driving.py
+++ driving.py
@@ -1,18 +1,20 @@
-country = input('請輸入你的國家: ')
-age = input('請輸入你的年齡: ')
-age = int(age)
-
-if country == '台灣' or country == '臺灣':
-	if age >= 18:
-		print('恭喜! 您可以考駕照')
+while True:
+	country = input('請輸入你的國家: ')
+	if country == '台灣' or country == '臺灣':
+		age = int(input('請輸入你的年齡: '))
+		if age >= 18:
+			print('恭喜! 您可以考駕照')
+			break
+		else:
+			print('您還不能考駕照')
+			break
+	elif country == '美國':
+		age = int(input('請輸入你的年齡: '))
+		if age >= 16:
+			print('恭喜! 您可以考駕照')
+			break
+		else:
+			print('您還不能考駕照')
+			break
 	else:
-		print('您還不能考駕照')
-
-elif country == '美國':
-	if age >= 16:
-		print('恭喜! 您可以考駕照')
-	else:
-		print('您還不能考駕照')
-
-else:
-	print('國家只能輸入台灣或美國')
+		print('國家只能輸入 台灣/美國')
