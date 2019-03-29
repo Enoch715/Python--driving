e351fe373b1b66688cee2338430b3dd7c855053c
CHENGHSIEN Lo <aszx99370765@gmail.com>
2019-03-29 14:49:52 +0800

    add USA

--- driving.py
+++ driving.py
@@ -1,7 +1,15 @@
 country = input('請輸入你的國家: ')
-age = int (input('請輸入你的年齡: '))
-if country == '台灣' or '臺灣':
-	if age < 18:
+age = input('請輸入你的年齡: ')
+age = int(age)
+
+if country == '台灣' or country == '臺灣':
+	if age >= 18:
+		print('恭喜! 您可以考駕照')
+	else:
 		print('您還不能考駕照')
-	elif age >= 18:
-		print ('恭喜! 您可以考駕照')
+
+elif country == '美國':
+	if age >= 16:
+		print('恭喜! 您可以考駕照')
+	else:
+		print('您還不能考駕照')
